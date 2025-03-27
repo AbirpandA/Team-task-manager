76f4a0f10adfbb31beac6bf9986d984c2889be49
Abir panda <abirpanda335@gmail.com>
2025-03-27 09:46:14 +0530

    did some improvements in the code

--- backend/src/middleware/userAuth.js
+++ backend/src/middleware/userAuth.js
@@ -1,37 +1,40 @@
-const joi = require('joi');
+const joi = require("joi");
 
-const signupValidation=(req,res,next)=>{
-     const Schema = joi.object.keys({
-        username: joi.string().required(),
-        email: joi.string().email().required(),
-        password: joi.string().required(),
+const signupValidation = (req, res, next) => {
+  const Schema = joi.object({
+    username: joi.string().min(3).max(30).required(),
+    email: joi.string().email().required(),
+    password: joi
+      .string()
+      .min(6) 
+      .pattern(new RegExp("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*#?&]{6,}$"))
+      .required()
+      .messages({
+        "string.pattern.base":
+          "Password must contain at least one letter and one number.",
+      }),
+  });
 
-     });
+  const { error } = Schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
 
-     const {error} = Schema.validate(req.body);
-     if(error){
-        return res.json({message: error.message});
-     };
-
-     next();
-
-
-
-
+  next();
 };
 
 const loginValidation = (req, res, next) => {
-    const Schema = joi.object().keys({
-        email: joi.string().email().required(),
-        password: joi.string().required()
-    });
+  const Schema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+  });
 
-    const {error} = Schema.validate(req.body);
-    if(error){
-        return res.json({message:error.message});
-    };
+  const { error } = Schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
 
-    next();
+  next();
 };
 
-module.exports = {signupValidation, loginValidation};
+module.exports = { signupValidation, loginValidation };
